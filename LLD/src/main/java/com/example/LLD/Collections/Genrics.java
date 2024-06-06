6b89d46582b54a045965810e44268f3ac97b605f
rajkamalkumar <rajkamalkumar207@gmail.com>
2024-06-06 15:46:16 +0530

    06-June 2024 unmarkedSumArray done

--- LLD/src/main/java/com/example/LLD/Collections/Genrics.java
+++ LLD/src/main/java/com/example/LLD/Collections/Genrics.java
@@ -1,7 +1,5 @@
 package com.example.LLD.Collections;
-/*
-Example of generic class
-*/
+
 class Pair<T,V> {
     // An object of type T is declared
     T obj1;
@@ -9,18 +7,12 @@ class Pair<T,V> {
     Pair(T obj1,V obj2) { this.obj1 = obj1;  this.obj2=obj2; } // constructor
     public T getObject1() { return this.obj1; }
     public V getObject2() { return this.obj2; }
-}
 
-// example of generic class with inheritance
-//generics with bound
-class  ChildPair<T extends  Number,V extends String> extends Pair {
-    ChildPair(T obj1, V obj2) {
-        super(obj1, obj2);
+    public T getKey() {
+        return getObject1();
+    }
+    public  V getValue() {
+        return getObject2();
     }
 }
 
-// example of generic method
-class GenericMethodExample{
-    public <p1,p2>  void display(p1 value1,p2 value2){
-    }
-}
--- LLD/src/main/java/com/example/LLD/Collections/Main.java
+++ LLD/src/main/java/com/example/LLD/Collections/Main.java
@@ -1,16 +1,59 @@
 package com.example.LLD.Collections;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 // Driver class to test above
 class Main {
     public static void main(String[] args) {
-        Pair<String,Integer> p1=new Pair<>("RAJKAMAL",345);
-        ChildPair childPair =new ChildPair(124,"string");
-        GenericMethodExample genericMethodExample=new GenericMethodExample();
-        genericMethodExample.display("string","874t8348");
-        Pizza pizza=new Pizza();
-        System.out.println(pizza.getStatus());
+//        Pair<String,Integer> p1=new Pair<>("RAJKAMAL",345);
+//        ChildPair childPair =new ChildPair(124,"string");
+//        GenericMethodExample genericMethodExample=new GenericMethodExample();
+//        genericMethodExample.display("string","874t8348");
+//        Pizza pizza=new Pizza();
+//        System.out.println(pizza.getStatus());
+//        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
 
+        int [] nums = {1,2,2,1,2,3,1};
+        int [][]queries = {{1,2},{3,3},{4,2}};
+
+        Solution solution=new Solution();
+        solution.unmarkedSumArray(nums,queries);
     }
 }
 
+
+class Solution {
+    public long[] unmarkedSumArray(int[] nums, int[][] queries) {
+        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>((a,b) -> {//value,index
+            if(a.getKey().intValue() == b.getKey().intValue())
+                return a.getValue().intValue() - b.getValue().intValue();
+            else
+                return a.getKey().intValue()-b.getKey().intValue();
+        });
+        int[] markedIndexs = new int[nums.length];
+        long sum = 0;
+        long[] result = new long[queries.length];
+        for(int i=0;i<nums.length;i++){
+            pq.offer(new Pair<>(nums[i],i));
+            sum += (long)nums[i];
+        }
+        for(int i = 0; i< queries.length;i++){
+            if(markedIndexs[queries[i][0]] == 0){
+                markedIndexs[queries[i][0]] =1;
+                sum-=(long)nums[queries[i][0]];
+            }
+            int k = queries[i][1];
+            while(k>0 && !pq.isEmpty()){
+                var min = pq.poll();
+                if(markedIndexs[min.getValue().intValue()] == 0){
+                    markedIndexs[min.getValue().intValue()] = 1;
+                    sum -= (long)nums[min.getValue().intValue()];
+                    k--;
+                }
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+}
